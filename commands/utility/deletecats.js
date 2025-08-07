3a3a8de01131e89d8aa3e26984c110ea9af206c8
Maringo <maringo@maringo.nl>
2025-08-07 19:18:44 +0200

    deletecats only for me

--- commands/utility/deletecats.js
+++ commands/utility/deletecats.js
@@ -1,21 +1,23 @@
-const {SlashCommandBuilder, MessageFlags, PermissionFlagsBits} = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const { devId } = require('./../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('deletecats')
-		.setDescription('Delete WW cats')
-		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+		.setDescription('Delete WW cats'),
 	async execute(interaction) {
-		const userdata = interaction.member;
-		const isAdmin = userdata.roles.cache.some(role => role.name === 'Admin');
 
-		if (isAdmin) {
+		// For me only.
+		if (interaction.member.id === devId) {
 			await interaction.reply({ content: "Deleting WW categories" });
 
 			const cats = interaction.guild.channels.cache.filter(CategoryChannel => CategoryChannel.parentId === null);
 
 			cats.each(cat => {
 				if (cat.name.slice(0, 2) === "WW") {
+					cat.children.cache.each(child => {
+						interaction.guild.channels.delete(child.id);
+					});
 					interaction.guild.channels.delete(cat.id);
 				}
 			});
